bad550f84165743d4a9a0e8d246206d363f625f7
Letícia Coelho <126501559+LeticiaCoelhoF@users.noreply.github.com>
2024-12-03 00:45:53 +0000

    HOME-projetoDiogo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,45 +1,65 @@
-import { Component } from '@angular/core';
-import { AuthenticateService } from '../services/auth.service';
+import { Component, OnInit } from '@angular/core';
 import { CrudService } from '../services/crud.service';
-import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
-import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  modelo: string = '';
+  marca: string = '';
+  anoFabricacao: number | null = null;
+  tipo: string = '';
+  mostrarLista: boolean = false;
 
-    isLoading: boolean = false;
-    funcionarios: any;
+  lista: { 
+    id: number; 
+    modelo: string; 
+    marca: string; 
+    ano: number; 
+    tipo: string 
+  }[] = []; // Lista vazia
 
-    getFuncionarios(){
-      this.isLoading = true;
-    
-      let funcionario = { CodFun: '6' };
+  constructor() {}
 
-      let funcionarios:any;
+  ngOnInit() {}
 
-      fetch('http://localhost/api/v1/listar_funcionarios.php',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(funcionario)
-        }
-      )
-      .then(response => response.json())
-      .then(response => {
-        console.log(response.funcionarios);
-        this.funcionarios = response.funcionarios;
-      })
-      .catch(erro => {
-        console.log(erro);
-      })
-      .finally(()=>{
-        this.isLoading = false;
-      })
+  consultar() {
+    // Exibe a lista quando o botão Consultar for clicado
+    if (this.lista.length === 0) {
     }
+    this.mostrarLista = true;
+  }
+
+  cadastrar() {
+    if (this.modelo && this.marca && this.anoFabricacao && this.tipo) {
+      // Adiciona um novo item à lista
+      this.lista.push({
+        id: this.lista.length + 1,
+        modelo: this.modelo,
+        marca: this.marca,
+        ano: this.anoFabricacao,
+        tipo: this.tipo,
+      });
+
+      // Limpa os campos do formulário após o cadastro
+      this.modelo = '';
+      this.marca = '';
+      this.anoFabricacao = null;
+      this.tipo = '';
+    } else {
+      alert('Por favor, preencha todos os campos antes de cadastrar.');
+    }
+  }
+
+  deletar(id: number) {
+    // Remove um item da lista pelo ID
+    this.lista = this.lista.filter((item) => item.id !== id);
+
+    // Se a lista ficar vazia após deletar, oculta a exibição
+    if (this.lista.length === 0) {
+      this.mostrarLista = false;
+    }
+  }
 }
