7f3060d9adac6ca0a06ea223f2a0cf15df524062
Letícia Coelho <126501559+LeticiaCoelhoF@users.noreply.github.com>
2024-12-03 18:34:15 +0000

    HOME-projeto 03-12

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,60 +6,44 @@ import { CrudService } from '../services/crud.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage implements OnInit {
-  modelo: string = '';
-  marca: string = '';
-  anoFabricacao: number | null = null;
-  tipo: string = '';
-  mostrarLista: boolean = false;
+  contato: any = {
+    modelo: null,
+    marca: null,
+    anoFabricacao: null,
+    tipo: null
+  };
+  id: any;
+  contatos: any;
 
-  lista: { 
-    id: number; 
-    modelo: string; 
-    marca: string; 
-    ano: number; 
-    tipo: string 
-  }[] = []; // Lista vazia
-
-  constructor() {}
-
-  ngOnInit() {}
-
-  consultar() {
-    // Exibe a lista quando o botão Consultar for clicado
-    if (this.lista.length === 0) {
-    }
-    this.mostrarLista = true;
+  constructor(
+    public crudService: CrudService
+  ) { 
+    this.getContatos();
   }
 
-  cadastrar() {
-    if (this.modelo && this.marca && this.anoFabricacao && this.tipo) {
-      // Adiciona um novo item à lista
-      this.lista.push({
-        id: this.lista.length + 1,
-        modelo: this.modelo,
-        marca: this.marca,
-        ano: this.anoFabricacao,
-        tipo: this.tipo,
-      });
-
-      // Limpa os campos do formulário após o cadastro
-      this.modelo = '';
-      this.marca = '';
-      this.anoFabricacao = null;
-      this.tipo = '';
-    } else {
-      alert('Por favor, preencha todos os campos antes de cadastrar.');
-    }
+  ngOnInit() {
   }
 
-  deletar(id: number) {
-    // Remove um item da lista pelo ID
-    this.lista = this.lista.filter((item) => item.id !== id);
-
-    // Se a lista ficar vazia após deletar, oculta a exibição
-    if (this.lista.length === 0) {
-      this.mostrarLista = false;
-    }
+  cadastrar(){
+    this.crudService.insert(this.contato, 'contatos');
+    this.getContatos();
   }
+
+  getContatos(){
+    this.crudService.fetchAll('contatos')
+    .then(dados => {
+      console.log(dados);
+      this.contatos = dados;
+    })
+  }
+
+  deletar(id: string){
+    this.crudService.remove(id, 'contatos')
+    .then(() => {
+      this.getContatos();
+    })
+  }
+  
 }
